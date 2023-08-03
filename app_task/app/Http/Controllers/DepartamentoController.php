2e79da64b099b38d798ce285d3f369be6acf360c
Douglas-00 <alvesdouglas547@gmail.com>
2023-08-02 21:01:22 -0300

    Validações fluxo e atribuição status Code -> JSON

--- app_task/app/Http/Controllers/DepartamentoController.php
+++ app_task/app/Http/Controllers/DepartamentoController.php
@@ -6,80 +6,91 @@ use App\Models\Departamento;
 use Illuminate\Http\Request;
 
 class DepartamentoController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+{   
+    public function __construct(Departamento $departamento){
+        $this->departamento = $departamento;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    //Exibindo Todos Departamentos
+    public function index()
+    {   
+        $departamentos = $this->departamento->all();
+        return response()->json($departamentos, 200);
+    }
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
-    {
-        //
+    {   
+        $departamento = $this->departamento->create($request->all());
+        return  response()->json($departamento, 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Departamento  $departamento
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Departamento $departamento)
-    {
-        //
+    //Exibindo departamento pelo id
+    public function show($id)
+    {   
+        try {
+
+            $departamento = $this->departamento->find($id);
+            if($departamento === null){
+                return response()->json (['erro' => 'Recurso pesquisado não existe'], 404);
+            }else{
+                return response()->json($departamento, 200);
+            }
+
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+        
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Departamento  $departamento
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Departamento $departamento)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Departamento  $departamento
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Departamento $departamento)
-    {
-        //
+    //Atualizando departamento
+    public function update(Request $request, $id)
+    {   
+        try {
+            
+            $departamento = $this->departamento->find($id);
+            if($departamento === null){
+                return response()->json(['erro' => 'Impossivel realizar a atualização, O recurso solicitado não existe.'], 404);
+            }else{
+                $departamento->update($request->all());
+
+                return response()->json($departamento, 200);
+            }
+            
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+        
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Departamento  $departamento
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Departamento $departamento)
-    {
-        //
+    //Deletando departamento pelo id
+    public function destroy($id)
+    {   
+        try {
+
+            $departamento = $this->departamento->find($id);
+            if($departamento === null){
+                return response()->json(['erro' => 'Impossivel realizar a exclusão, O recurso solicitado não existe.'], 404);
+            }else{
+                $departamento->delete();
+
+                return response()->json(['msg' =>'O Departamento foi removido com sucesso!!'], 200);
+            }
+
+            
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+        
+        
     }
 }
